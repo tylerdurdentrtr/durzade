490ace1ce7aacabf3b89c3de67a0f5db07b630f2
tylerdurden <tylerdurdentrtr@gmail.com>
2023-04-26 14:43:05 +0300

    .

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 var navTop = document.querySelector(".nav-top-wrapper");
-
-console.log(window.scrollY);
+var hMenuIcon = document.querySelector(".hm-icon");
+var hmClose = document.querySelector(".hm-close");
+var hMenuModal = document.querySelector(".h-menu-modal");
+var headerHmIcon = document.querySelector(".header-hm-icon");
 
 function showNavTop() {
   if (window.scrollY > 475) {
@@ -11,4 +13,22 @@ function showNavTop() {
   console.log(window.scrollY);
 }
 
+function openHmMenu() {
+  hMenuModal.style.display = "flex";
+  body.style.overflowY = "hidden";
+}
+function closeHmMenu() {
+  hMenuModal.style.display = "none";
+  body.style.overflowY = "scroll";
+}
+
+window.onclick = function (e) {
+  if (e.target == hMenuModal) {
+    hMenuModal.style.display = "none";
+  }
+};
+
+hMenuIcon.addEventListener("click", openHmMenu);
+hmClose.addEventListener("click", closeHmMenu);
+headerHmIcon.addEventListener("click", openHmMenu);
 window.addEventListener("scroll", showNavTop);
