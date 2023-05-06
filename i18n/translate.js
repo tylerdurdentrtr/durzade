8d2ce69aefbe4caf106bde9885840317d3c13691
tylerdurden <tylerdurdentrtr@gmail.com>
2023-05-06 13:13:44 +0300

    .

---
+++ i18n/translate.js
@@ -0,0 +1,31 @@
+import translations from "./translations.js";
+
+let titleElement = document.querySelector("head title");
+
+titleElement.textContent = titleElement.textContent.toLowerCase();
+
+if (titleElement.textContent === "durzade") titleElement.textContent = "home";
+
+let page = titleElement.textContent;
+
+console.log(page);
+
+const languageSelector = document.querySelector(".lang-selector");
+languageSelector.addEventListener("change", (event) => {
+  setLanguage(event.target.value);
+  localStorage.setItem("lang", event.target.value);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const language = localStorage.getItem("lang") || "en"; // اذا لم تكن اللغة متوفرة استخدم الانجليزية
+  setLanguage(language);
+});
+
+const setLanguage = (language) => {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-i18n");
+    element.textContent = translations[page][language][translationKey];
+  });
+  document.dir = language === "ar" ? "rtl" : "ltr";
+};
---
+++ i18n/translations.js
@@ -0,0 +1,225 @@
+const translations = {
+  home: {
+    en: {
+      english: "English",
+      arabic: "Arabic",
+      turkish: "Turkish",
+      home1: "About",
+      home2: "Products",
+      home3: "Services",
+      home4: "Contact",
+      home5: "Home",
+      home6: "",
+      home7: "",
+      home8: "See",
+      home9: "",
+      home10: "",
+      home11: "Knowledge & Consultancy",
+      home12: "Quality Controll",
+      home13: "Customer Care",
+      home14: "Capacity",
+      home15:
+        "A high storage capacity for a wide product range of colors and selections, for varieties of international marble, available in the sizes of slabs and standard tiles.",
+    },
+
+    ar: {
+      english: "الإنجليزية",
+      arabic: "العربية",
+      turkish: "تركي",
+      home1: "حول",
+      home2: "منتجات",
+      home3: "خدمات",
+      home4: "اتصل",
+      home5: "الصفحة الرئيسية",
+      home6: "",
+      home7: "",
+      home8: "انظر",
+      home9: "",
+      home10: "",
+      home11: "المعرفة والاستشارات",
+      home12: "مراقبة الجودة",
+      home13: "العناية بالعملاء",
+      home14: "القدرة",
+      home15:
+        "سعة تخزين عالية لمجموعة واسعة من الألوان والاختيارات لأصناف الرخام الدولي، متاحة في أحجام اللوحات والبلاطات القياسية.",
+    },
+
+    tr: {
+      english: "İngilizce",
+      arabic: "Arapça",
+      turkish: "Türkçe",
+      home1: "Hakkımızda",
+      home2: "Ürünler",
+      home3: "Hizmetler",
+      home4: "İletişim",
+      home5: "Ana Sayfa",
+      home6: "",
+      home7: "",
+      home8: "Görüntüle",
+      home9: "",
+      home10: "",
+      home11: "Bilgi ve Danışmanlık",
+      home12: "Kalite Kontrolü",
+      home13: "Müşteri Hizmetleri",
+      home14: "Kapasite",
+      home15:
+        "Uluslararası mermer çeşitlerinin slab ve standart fayans boyutlarında mevcut olduğu geniş bir renk ve seçenek yelpazesinde yüksek bir depolama kapasitesi.",
+    },
+  },
+  about: {},
+  products: {},
+  services: {
+    en: {
+      english: "English",
+      arabic: "Arabic",
+      turkish: "Turkish",
+      home1: "About",
+      home2: "Products",
+      home3: "Services",
+      home4: "Contact",
+      home5: "Home",
+      services1: "Services",
+      services2: "EXCLUSIVE SERVICES",
+      services3: "KNOWLEDGE & CONSULTANCY",
+      services4:
+        "As we have been working with more than 400 different natural stone selections so far, and with the help of a team disparsed around different locations in Turkey and the world, everyday information from the market and the quarries is being transmitted and analyzed for us to constantly grow our knowldge on the field so that we can offer our customers information in a timely manner to help source the materials within an optimal quality / price ratio.",
+      services5: "QUALITY CONTROL",
+      services6:
+        "In addition to the product sourcing and supply, we have gained the trust of customers in more than 50 different countries through our quality control service. In fact, not any order is loaded without being inspected by one of our experts and confirmed by the customer. In that way, we make sure we are fulfillling all our responsibilities regarding selections, packaging and load,ng conditions. Transparency is also important as we do not hesitate to warn the customer if any problem occurs prior to loading.",
+      services7: "CUSTOMER CARE",
+      services8:
+        " Advice & assistance throughout all the sales process ia another service that is important to us. We make sure to assist all our customers throughout the whole process, from product sourcing untill it reaches its final destination. All our departments make sure to gather their efforts so that we can offer our customers an optimal purchasing experience.",
+      services9: "STANDARD SERVICES",
+      services10: "TRANSPORTATION",
+      services11:
+        "We are working with professional worldwide shipping companies and sensitive about delivering the goods on time all ports of the world.",
+      services12: "PACKAGING",
+      services13:
+        " Every crate has info card that shows the selection, quality, total quantity, weight and material name. Every crate has info card that shows the selection, quality, total quantity, weight and material name.",
+      services14: "FUMIGATION",
+      services15:
+        " All the containers go through fumigation process. (with methyl bromide 68 gr/m³ for each container)",
+      services16: "LASHING",
+      services17:
+        "Lashing is done for all loadings, to secure goods against cracks and breaks during the transportation.",
+      services18: "HEAT TREATMENT",
+      services19:
+        " The wooden materials, which are used for lashing, go through heat treatment process against insects, mites and other hazardous organisms.",
+      services20: "LOADING",
+      services21:
+        "All the products such as blocks, slabs, tiles and mosaics are weighed before to get the exact weights.",
+      services22: "DOCUMENTATION",
+      services23:
+        "The shipping documents are completly prepared according to thebuyers request and we are very carefull to send to the buyerimmediately.",
+      services24: "PRODUCTION",
+      services25:
+        "All materials are throughly examined and checked during production.",
+      services26: "SELECTION",
+      services27:
+        "Tiles & Slabs are checked according to quality, colour variation, homogeneousness and are separated as by experienced workers.",
+    },
+
+    ar: {
+      english: "الإنجليزية",
+      arabic: "العربية",
+      turkish: "تركي",
+      home1: "حول",
+      home2: "منتجات",
+      home3: "خدمات",
+      home4: "اتصل",
+      home5: "الصفحة الرئيسية",
+      services1: "الخدمات",
+      services2: "الخدمات الحصرية",
+      services3: "المعرفة والاستشارات",
+      services4:
+        "نظرًا لأننا عملنا مع أكثر من ٤٠٠ خيارًا مختلفًا للحجر الطبيعي حتى الآن، وبمساعدة فريق متناثر في مواقع مختلفة في تركيا والعالم، يتم نقل وتحليل المعلومات اليومية من السوق والمحاجر لنمد علمنا باستمرار في هذا المجال حتى نتمكن من تزويد عملائنا بالمعلومات في الوقت المناسب للمساعدة في تحديد المواد بنسبة جودة / سعر مثلى.",
+      services5: "مراقبة الجودة",
+      services6:
+        "بالإضافة إلى توريد المنتجات، فقد كسبنا ثقة العملاء في أكثر من ٥٠ دولة مختلفة من خلال خدمة مراقبة الجودة. في الواقع، لا يتم تحميل أي طلب بدون فحص أحد خبرائنا وتأكيده من قبل العميل. وبهذه الطريقة، نتأكد من أننا نفي بجميع مسؤولياتنا المتعلقة بالاختيارات والتغليف وظروف التحميل. الشفافية مهمة أيضًا حيث لا نتردد في تحذير العميل إذا حدث أي مشكلة قبل التحميل.",
+      services7: "خدمة العملاء",
+      services8:
+        " النصيحة والمساعدة طوال عملية البيع خدمة أخرى مهمة بالنسبة لنا. نتأكد من مساعدة جميع عملائنا طوال العملية بدءًا من توريد المنتج حتى يصل إلى وجهته النهائية. يتأكد جميع أقسامنا من",
+      services9: "الخدمات القياسية",
+      services10: "النقل",
+      services11:
+        "نحن نعمل مع شركات شحن عالمية محترفة وحريصون على تسليم البضائع في الوقت المحدد إلى جميع موانئ العالم.",
+      services12: "التغليف",
+      services13:
+        "يحتوي كل صندوق على بطاقة معلومات توضح الاختيار والجودة والكمية الإجمالية والوزن واسم المادة. يحتوي كل صندوق على بطاقة معلومات توضح الاختيار والجودة والكمية الإجمالية والوزن واسم المادة.",
+      services14: "تبخير",
+      services15:
+        "تَمَرُ جَمِيعَ الْحَاوِيَاتُ بِعَمَلِيَّةِ التَّبْخِيرِ. (بِمَيْثِيلِ بُرومِيدِ ٦٨ جم / م ³ لِكُلِّ حَاوِيَةٍ)",
+
+      services16: "ربط الحمولة",
+      services17:
+        "يتم ربط الحمولة في جميع الشحنات لتأمين البضائع ضد التصدعات والكسور أثناء عملية النقل.",
+      services18: "معالجة الحرارة",
+      services19:
+        "تخضع المواد الخشبية المستخدمة لربط الحمولة لعملية معالجة حرارية ضد الحشرات والعث والكائنات الخطيرة الأخرى.",
+      services20: "التحميل",
+      services21:
+        "يتم وزن جميع المنتجات مثل الكتل والألواح والبلاط والفسيفساء قبل الشحن للحصول على الأوزان الدقيقة.",
+      services22: "التوثيق",
+      services23:
+        "يتم إعداد جميع وثائق الشحن بالكامل وفقًا لطلب المشتري ونحرص كثيرًا على إرسالها إلى المشتري على الفور.",
+      services24: "الإنتاج",
+      services25: "يتم فحص جميع المواد بعناية وفحصها خلال الإنتاج.",
+      services26: "الاختيار",
+      services27:
+        "يتم فحص البلاط والألواح وفقًا للجودة وتباين اللون والتجانس ويتم فصلها من قبل العمال ذوي الخبرة.",
+    },
+
+    tr: {
+      english: "İngilizce",
+      arabic: "Arapça",
+      turkish: "Türkçe",
+      home1: "Hakkımızda",
+      home2: "Ürünler",
+      home3: "Hizmetler",
+      home4: "İletişim",
+      home5: "Ana Sayfa",
+      services1: "Hizmetler",
+      services2: "ÖZEL HİZMETLER",
+      services3: "BİLGİ & DANIŞMANLIK",
+      services4:
+        "Bugüne kadar 400'den fazla doğal taş seçeneği ile çalışıyoruz ve Türkiye ve dünyanın farklı yerlerinde bulunan bir ekip sayesinde, pazardan ve ocaklardan her gün gelen bilgileri ileterek ve analiz ederek, müşterilerimize malzemeyi en uygun kalite / fiyat oranı içinde kaynaklandırmak için sürekli olarak bilgi birikimimizi arttırıyoruz.",
+      services5: "KALİTE KONTROLÜ",
+      services6:
+        "Ürün kaynaklama ve tedariki yanı sıra, kalite kontrol hizmetimiz aracılığıyla 50'den fazla farklı ülkeden müşterilerimizin güvenini kazandık. Aslında, herhangi bir sipariş, uzmanlarımızdan biri tarafından incelenmeden ve müşteri tarafından onaylanmadan yüklenmiyor. Bu şekilde, seçimler, paketleme ve yükleme koşulları konusundaki sorumluluklarımızı yerine getirdiğimizden emin oluyoruz. Ayrıca şeffaflık da önemlidir, herhangi bir sorun oluşursa yüklemeden önce müşteriyi uyarmanın çekinmiyoruz.",
+      services7: "MÜŞTERİ HİZMETLERİ",
+      services8:
+        "Tüm satış süreci boyunca tavsiye ve yardım sağlamak da bizim için önemli bir hizmettir. Ürün kaynaklandırmadan son varış yerine kadar tüm müşterilerimize yardımcı olmaktan emin oluyoruz. Tüm departmanlarımız çabalarını birleştirerek müşterilerimize optimal bir satın alma deneyimi sunabilmek için çalışıyorlar.",
+      services9: "HİZMETLERİMİZ",
+      services10: "TAŞIMACILIK",
+      services11:
+        "Profesyonel dünya çapında nakliye şirketleri ile çalışıyoruz ve dünya limanlarına zamanında mal teslim etme konusunda hassasız.",
+      services12: "AMBALAJLAMA",
+      services13:
+        "Her kasa, seçim, kalite, toplam miktar, ağırlık ve malzeme adını gösteren bir bilgi kartı içerir. Her kasa, seçim, kalite, toplam miktar, ağırlık ve malzeme adını gösteren bir bilgi kartı içerir.",
+      services14: "FUMİGASYON",
+      services15:
+        "Tüm konteynerler, her bir konteyner için metil bromür 68 gr/m³ ile fümigasyon sürecinden geçer.",
+      services16: "LASHİNG",
+      services17:
+        "Tüm yüklemeler için lashing yapılarak, malzemelerin taşıma sırasında çatlama ve kırılmalara karşı güvenliği sağlanır.",
+      services18: "ISI İŞLEMİ",
+      services19:
+        "Lashing için kullanılan ahşap malzemeler, böcekler, akarlar ve diğer zararlı organizmalara karşı ısı işlemi sürecinden geçer.",
+      services20: "YÜKLEME",
+      services21:
+        "Bloklar, levhalar, karo ve mozaikler gibi tüm ürünler, tam ağırlıklarını almak için tartılır.",
+      services22: "BELGELENDİRME",
+      services23:
+        "Nakliye belgeleri alıcının isteğine göre tamamen hazırlanır ve alıcıya hemen gönderilmeye özen gösterilir.",
+      services24: "ÜRETİM",
+      services25:
+        "Tüm malzemeler üretim sırasında dikkatle incelenir ve kontrol edilir.",
+      services26: "SEÇİM",
+      services27:
+        "Karo ve levhalar, kalite, renk varyasyonu, homojenlik açısından deneyimli işçiler tarafından kontrol edilir ve ayrılır.",
+    },
+  },
+  contact: {},
+};
+
+export default translations;
