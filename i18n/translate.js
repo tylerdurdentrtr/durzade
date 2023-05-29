3b2d9cdf146ecc6dfb30be0b6ce9e0364d2dda27
tylerdurden <tylerdurdentrtr@gmail.com>
2023-05-29 22:40:28 +0300

    .

--- i18n/translate.js
+++ i18n/translate.js
@@ -11,6 +11,25 @@ if (page === "durzade") page = "home";
 console.log(page);
 
 const languageSelectors = document.querySelectorAll(".lang-selector");
+// const options = document.querySelectorAll(".lang-selector options");
+
+const options = {
+  en: [],
+  ar: [],
+  tr: [],
+};
+
+languageSelectors.forEach((e) => {
+  options.en.push(e.childNodes[1]);
+  options.ar.push(e.childNodes[3]);
+  options.tr.push(e.childNodes[5]);
+});
+
+const localStorageLang = localStorage.getItem("lang");
+
+if (localStorageLang === "en") options.en.forEach((e) => (e.selected = true));
+if (localStorageLang === "ar") options.ar.forEach((e) => (e.selected = true));
+if (localStorageLang === "tr") options.tr.forEach((e) => (e.selected = true));
 
 languageSelectors.forEach((languageSelector) => {
   languageSelector.addEventListener("change", (event) => {
